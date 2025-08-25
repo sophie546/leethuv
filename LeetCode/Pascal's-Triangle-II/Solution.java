ac6146d40cbd53d0d800c3dc99b8ac79e94eaf3b
sophie546 <133882510+sophie546@users.noreply.github.com>
2025-08-25 23:20:28 +0800

    Add solution for Pascal's Triangle II

--- LeetCode/Pascal's-Triangle-II/Solution.java
+++ LeetCode/Pascal's-Triangle-II/Solution.java
@@ -1,24 +1,20 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        List<List<Integer>> triangle = new ArrayList<>();
+       List<List<Integer>> triangle = new ArrayList<>();
+       triangle.add(new ArrayList<>());
+       triangle.get(0).add(1);
 
-        triangle.add(new ArrayList<>());
-        triangle.get(0).add(1);
-
-        if(rowIndex == 0) return triangle.get(0);
-
-        for(int row = 1; row <= rowIndex; row++){
+       for(int row = 1; row <= rowIndex; row++){
             List<Integer> prevRow = triangle.get(row - 1);
             List<Integer> newRow = new ArrayList<>();
             newRow.add(1);
 
-            for(int j = 1; j<row; j++){
+            for(int j=1; j < row; j++){
                 newRow.add(prevRow.get(j - 1) + prevRow.get(j));
             }
             newRow.add(1);
             triangle.add(newRow);
-        }
+       }
         return triangle.get(rowIndex);
-
     }
 }
