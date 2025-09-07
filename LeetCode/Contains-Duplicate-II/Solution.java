1540d31826377c943267fe2df3a518395bc1f97c
sophie546 <133882510+sophie546@users.noreply.github.com>
2025-09-07 22:26:43 +0800

    Add solution for Contains Duplicate II

--- LeetCode/Contains-Duplicate-II/Solution.java
+++ LeetCode/Contains-Duplicate-II/Solution.java
@@ -1,16 +1,1 @@
-class Solution {
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
-       HashSet<Integer> set = new HashSet<>();
-       for(int i = 0; i < nums.length; i++){
-            if(set.contains(nums[i])){
-                return true;
-            }
-            set.add(nums[i]);
-            if(set.size() > k){
-                set.remove(nums[i - k]);
-            }
-
-       }
-       return false;
-    }
-}
+if (n < k) return false;
