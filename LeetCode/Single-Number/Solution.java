c535042fe6aa315cc320b6a5ced825a721ce15d6
sophie546 <133882510+sophie546@users.noreply.github.com>
2025-09-08 21:58:45 +0800

    Add solution for Single Number

--- LeetCode/Single-Number/Solution.java
+++ LeetCode/Single-Number/Solution.java
@@ -1,16 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        int sumAll =0, uniqueSum =0;
-
+        int result =0;
         for(int num : nums){
-            if(!set.contains(num)){
-                set.add(num);
-                uniqueSum += num;
-            }
-            sumAll += num;
+            result ^= num;
         }
-
-        return 2 * uniqueSum - sumAll;
+        return result;
     }
 }
