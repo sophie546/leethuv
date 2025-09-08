9c11feee175ff0831708af9436cb8e5d3cdc36a7
sophie546 <133882510+sophie546@users.noreply.github.com>
2025-09-08 22:57:38 +0800

    Add solution for Convert Integer to the Sum of Two No-Zero Integers

--- LeetCode/Convert-Integer-to-the-Sum-of-Two-No-Zero-Integers/Solution.java
+++ LeetCode/Convert-Integer-to-the-Sum-of-Two-No-Zero-Integers/Solution.java
@@ -1,15 +1,16 @@
 class Solution {
     public int[] getNoZeroIntegers(int n) {
-        int[] num = new int[2];
-        for(int i=1; i < n; i++){
+       int[] num = new int[2];
+       for(int i = 1; i < n; i++){
             int a = i;
-            int b = n - a;
+            int b = n - i;
             if(isNoZero(a) && isNoZero(b)){
                 num[0] = a;
                 num[1] = b;
+                return num;
             }
-        }
-        return num;
+       }
+       return num;
     }
 
     public boolean isNoZero(int x){
@@ -21,4 +22,5 @@ class Solution {
         }
         return true;
     }
+    
 }
