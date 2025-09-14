66968aa83de3dc786b0cdfc3c15c4a9dab0537f5
sophie546 <133882510+sophie546@users.noreply.github.com>
2025-09-14 23:29:59 +0800

    Add solution for Intersection of Two Arrays II

--- LeetCode/Intersection-of-Two-Arrays-II/Solution.java
+++ LeetCode/Intersection-of-Two-Arrays-II/Solution.java
@@ -1,25 +1,25 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        
-        for(int num : nums1){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
+       HashMap<Integer,Integer> map = new HashMap<>();
 
-        List<Integer> resTemp = new ArrayList<>();
-        for(int num : nums2){
-            if(map.containsKey(num) && map.get(num) > 0){
-                resTemp.add(num);
-                map.put(num, map.get(num) - 1);
-            }
-        }
+       for(int num : nums1){
+        map.put(num, map.getOrDefault(num,0)+1);
+       }
 
-        int[] res = new int[resTemp.size()];
-        int index = 0;
-        for(int num : resTemp){
-            res[index++] = num;
+        List<Integer> lis = new ArrayList<>();
+       for(int num : nums2){
+        if(map.containsKey(num) && map.get(num) > 0){
+            lis.add(num);
+            map.put(num,map.get(num) - 1);
         }
-        
-        return res;
+       }
+
+       int[] res = new int[lis.size()];
+       int index = 0;
+       for(int num : lis){
+        res[index++] = num;
+       }
+
+       return res;
     }
 }
