eb4dfe0e2bc74d75b9ad3dfd6d12ddb9add7557e
sophie546 <133882510+sophie546@users.noreply.github.com>
2025-09-17 23:03:32 +0800

    Add solution for Third Maximum Number

--- LeetCode/Third-Maximum-Number/Solution.java
+++ LeetCode/Third-Maximum-Number/Solution.java
@@ -1,20 +1,20 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        int res = 0;
+      HashSet<Integer> set = new HashSet<>();
+      int res = 0;
+      for(int num : nums){
+        set.add(num);
+      }
 
-        for(int num : nums){
-            set.add(num);
+      if(set.size() < 3){
+        for(int num : set){
+            res = Math.max(res, num);
         }
-            
-        if(set.size() < 3){
-            for(int num : set){
-                res = Math.max(res,num);
-            }
-            return res;
-        }
-        List<Integer> list = new ArrayList<>(set);
-        Collections.sort(list, Collections.reverseOrder());
-        return list.get(2);
+        return res;
+      }
+      List<Integer> list = new ArrayList<>(set);
+      Collections.sort(list, Collections.reverseOrder());
+      return list.get(2);
+      
     }
 }
