eb2c33c899ab9416dd2eb29e9823452921ad9306
sophie546 <133882510+sophie546@users.noreply.github.com>
2025-09-24 01:13:17 +0800

    Add solution for N-Repeated Element in Size 2N Array

---
+++ LeetCode/N-Repeated-Element-in-Size-2N-Array/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : nums){
+            if(!set.contains(num)){
+            set.add(num);
+            } else {
+            return num;
+            }
+        }
+
+        return 0;
+    }
+}
