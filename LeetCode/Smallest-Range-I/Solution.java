66435f6fe375d7e188ef9bf355c1572d1fcc9182
sophie546 <133882510+sophie546@users.noreply.github.com>
2025-09-26 01:40:27 +0800

    Add solution for Smallest Range I

--- LeetCode/Smallest-Range-I/Solution.java
+++ LeetCode/Smallest-Range-I/Solution.java
@@ -1,21 +1,17 @@
 class Solution {
     public int smallestRangeI(int[] nums, int k) {
-       
-        int maxVal = Integer.MIN_VALUE;
-        int minVal = Integer.MAX_VALUE;
+       int maxVal = Integer.MIN_VALUE;
+       int minVal = Integer.MAX_VALUE;
 
-        for (int num : nums) {
-            if (num > maxVal) {
+       for(int num : nums){
+            if(maxVal < num){
                 maxVal = num;
-            }
-            if (num < minVal) {
+            } 
+            if(minVal > num){
                 minVal = num;
             }
-        }
-
-       
-        int score = (maxVal - minVal) - 2 * k;
-
-        return Math.max(0, score);
+       }
+        int score = (maxVal - minVal) - 2*k;
+        return Math.max(0,score);     
     }
 }
