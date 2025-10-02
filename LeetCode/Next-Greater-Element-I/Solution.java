66901f89a7a3f9a1080b4b58e4b0be806be32982
sophie546 <133882510+sophie546@users.noreply.github.com>
2025-10-03 00:15:13 +0800

    Add solution for Next Greater Element I

--- LeetCode/Next-Greater-Element-I/Solution.java
+++ LeetCode/Next-Greater-Element-I/Solution.java
@@ -13,6 +13,7 @@ class Solution {
                             break;
                         }
                    }
+                   break;
                 }
             }
             index++;
