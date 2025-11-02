9acdc5dca2ec405f27ded8849b21eef34766787b
sophie546 <133882510+sophie546@users.noreply.github.com>
2025-11-03 01:32:47 +0800

    Add solution for Count Unguarded Cells in the Grid

--- LeetCode/Count-Unguarded-Cells-in-the-Grid/Solution.java
+++ LeetCode/Count-Unguarded-Cells-in-the-Grid/Solution.java
@@ -1,46 +1,42 @@
-class Solution {
-    public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        int[][] grid = new int[m][n];
-
-        for(int[] g : guards){
-            grid[g[0]][g[1]] = 1;
-        }
-
-        for(int[] w : walls){
-            grid[w[0]][w[1]] = 1;
-        }
-
-        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
-
-        for(int[] g : guards){
-            for(int[] d : directions){
-                int x = g[0];
-                int y = g[1];
-
-                while(true){
-                    x += d[0];
-                    y += d[1];
-
-                    if(x < 0 || x >= m || y < 0 || y >= n) break;
-                    if(grid[x][y] == 1 || grid[x][y] == 2) break;
-
-                    if(grid[x][y] == 0){
-                        grid[x][y] = 3;
-                    }
-                }
-            }
-        }
-
-        
-        int count = 0;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j<n;j++){
-                if(grid[i][j] == 0){
-                    count++;
-                }
-            }
-        }
-
-        return count;
-    }
-}
+    obstacles_in_row = [[] for _ in range(m)] # i -> list of walls and guards in that row sorted by j
+    obstacles_in_col = [[] for _ in range(n)] # j -> list of walls and guards in that row sorted by i
+    for i, j in guards:
+        obstacles_in_row[i].append((j, "G"))
+        obstacles_in_col[j].append((i, "G"))
+    for i, j in walls:
+        obstacles_in_row[i].append((j, "W"))
+        obstacles_in_col[j].append((i, "W"))
+    for row in obstacles_in_row:
+        row.sort()
+    for col in obstacles_in_col:
+        col.sort()
+    
+    obstacles = {(i, j) for i, j in chain(guards, walls)}
+    ans = 0
+    for i in range(m):
+        for j in range(n):
+            if (i, j) in obstacles:
+                continue
+            
+            row = obstacles_in_row[i]
+            if row:
+                east = bisect_left(row, j, key=itemgetter(0))
+                west = east - 1
+                if (
+                    (west >= 0 and row[west][1] == "G")
+                    or (east < len(row) and row[east][1] == "G")
+                ):
+                    continue
+            
+            col = obstacles_in_col[j]
+            if col:
+                south = bisect_left(col, i, key=itemgetter(0))
+                north = south - 1
+                if (
+                    (north >= 0 and col[north][1] == "G")
+                    or (south < len(col) and col[south][1] == "G")
+                ):
+                    continue
+            
+            ans += 1
+    return ans
