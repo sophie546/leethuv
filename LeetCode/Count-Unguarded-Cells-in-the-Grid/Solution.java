fe65a861d3557218b4cf1add751579dadee6ff34
sophie546 <133882510+sophie546@users.noreply.github.com>
2025-11-03 01:06:24 +0800

    Add solution for Count Unguarded Cells in the Grid

---
+++ LeetCode/Count-Unguarded-Cells-in-the-Grid/Solution.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
+        int[][] grid = new int[m][n];
+
+        for(int[] g : guards){
+            grid[g[0]][g[1]] = 1;
+        }
+
+        for(int[] w : walls){
+            grid[w[0]][w[1]] = 1;
+        }
+
+        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        for(int[] g : guards){
+            for(int[] d : directions){
+                int x = g[0];
+                int y = g[1];
+
+                while(true){
+                    x += d[0];
+                    y += d[1];
+
+                    if(x < 0 || x >= m || y < 0 || y >= n) break;
+                    if(grid[x][y] == 1 || grid[x][y] == 2) break;
+
+                    if(grid[x][y] == 0){
+                        grid[x][y] = 3;
+                    }
+                }
+            }
+        }
+
+        
+        int count = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j<n;j++){
+                if(grid[i][j] == 0){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
