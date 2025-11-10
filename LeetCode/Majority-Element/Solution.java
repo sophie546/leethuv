78d9b99b71bf2821ba4c1554026db0d0a1d1b4a8
sophie546 <133882510+sophie546@users.noreply.github.com>
2025-11-10 18:41:40 +0800

    Add solution for Majority Element

--- LeetCode/Majority-Element/Solution.java
+++ LeetCode/Majority-Element/Solution.java
@@ -1,18 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int first = nums[0];
-        int count =1;
-        for(int i=1; i < nums.length; i++){
+       int first = nums[0];
+       int count = 1;
+       for(int i = 1; i < nums.length; i++){
             if(first == nums[i]){
                 count++;
             } else{
                 count--;
                 if(count == 0){
-                    count=1;
+                    count = 1;
                     first = nums[i];
                 }
             }
-        }
-        return first;
+       }
+       return first;
     }
 }
