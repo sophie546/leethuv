eacb9ccbbc3e17c970b6ab9264065a1ba837d8d7
sophie546 <133882510+sophie546@users.noreply.github.com>
2026-04-04 21:47:18 +0800

    Add solution for Decode the Slanted Ciphertext

---
+++ LeetCode/Decode-the-Slanted-Ciphertext/Solution.py
@@ -0,0 +1,16 @@
+"bzqlagy                gxwd tx                uvqinyi                jjmdhhb                kjcm la                rxwcqnf                ycuxczg                wwnvnqz                bsdqkqr                ushfkzu                heny dw                qvbuagj                blawlga                wzhgxxg                tzb thk                shtxjof                feguqx"
+17
+"osljjaooouqphokrnf     lsdoioccbdhbsqkm b     qmqoitpqnpqwnebsou     llvfotmazegriuigrs     vjpbgaqifwo  kaqto     dsupahycdgbyoubsu"
+6
+"iveo    eed   l t    olc"
+4
+"zcnbsyedjgeiu ywwdge hygibdagfnybwif j lpfelxkyuizfzlrgpcvkrcahvzecgtqmjetyqbexqvsckynvgcandbu zlkhjc"
+1
+"wmihfwf bddhzaizuzhbuoovyyjstardqceaqzafdzihjbj ywly amkeemr jmvsfaavbpgiafgxzciwmrrtasthc hqfrtwoizoilw"
+2
+"suyexvjtk             lduoqccop             mbxotagus             ibrnbyxhs             ttxjbhkmy             inqntngxg             alqvlwsgg             z lxfupvz             ofpqlhydq             iztogeqis             pdtrqnuhu             sqjfwdyhd             wkvegwkoy             wa smlty"
+14
+"issqcpi                  wjgpvbw                  mncblia                  rkujbgs                  ivvaulw                  bcspydl                  iokjaka                  cic zne                  xmwcnnr                  t bujij                  wlrrvvi                  nkkbgxy                  plwtxab                  vku pff                  khqszzb                  ellpduv                  ndmghwq                  sqzgvhb                  vxzibo"
+19
+"             a     b"
+4
