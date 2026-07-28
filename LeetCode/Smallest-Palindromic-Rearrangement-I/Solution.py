d480ae56f7028b640a5da7c8580b320fff3a1c74
sophie546 <133882510+sophie546@users.noreply.github.com>
2026-07-28 23:10:53 +0800

    Add solution for Smallest Palindromic Rearrangement I

--- LeetCode/Smallest-Palindromic-Rearrangement-I/Solution.py
+++ LeetCode/Smallest-Palindromic-Rearrangement-I/Solution.py
@@ -1,9 +1,9 @@
-class Solution:
-    def smallestPalindrome(self, s: str) -> str:
-        n = len(s)
-        freq = Counter(s[:n >> 1])
-        
-        half = "".join(c * freq[c] for c in ascii_lowercase)
-        mid = s[n >> 1] if n & 1 else ""
-        
-        return half + mid + half[::-1]
+1class Solution:
+2    def smallestPalindrome(self, s: str) -> str:
+3        n = len(s)
+4        freq = Counter(s[:n >> 1])
+5        
+6        half = "".join(c * freq[c] for c in ascii_lowercase)
+7        mid = s[n >> 1] if n & 1 else ""
+8        
+9        return half + mid + half[::-1]
